c578ef0a24e482dd0513f4ec4d5c3cd5bb6d3c81
Stephen Buchanan <stephen@pop-os.localdomain>
2023-03-20 17:23:41 +0100

    working with complete, but progress stops complete update

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
-use audio::Host;
 use nannou::prelude::*;
 use nannou_audio as audio;
 use nannou_audio::Buffer;
 
+use std::sync::mpsc::{self, Receiver, Sender};
+
 struct Model {
-    audio_host: Host,
-    streams: Vec<audio::Stream<Audio>>,
+    receiver: Receiver<AudioClipInfo>,
+    stream: audio::Stream<Audio>,
+    clips: Vec<AudioClipInfo>,
 }
 
-type Callback = fn();
+#[derive(Debug, PartialEq)]
+enum AudioState {
+    Ready(),
+    Playing(f32),
+    Complete(),
+}
+
+struct AudioClip {
+    id: usize,
+    state: AudioState,
+    reader: audrey::read::BufFileReader,
+}
+
+#[derive(Debug)]
+struct AudioClipInfo {
+    id: usize,
+    state: AudioState,
+}
+
 struct Audio {
-    sound: audrey::read::BufFileReader,
-    is_completed: bool,
-    on_completed: Callback,
+    sounds: Vec<AudioClip>,
+    sender: Sender<AudioClipInfo>,
 }
 
 fn main() {
@@ -30,20 +49,36 @@ fn model(app: &App) -> Model {
     // Initialise the audio host so we can spawn an audio stream.
     let audio_host = audio::Host::new();
 
+    let (tx, rx) = mpsc::channel::<AudioClipInfo>();
+    let audio_model = Audio {
+        sounds: Vec::new(),
+        sender: tx,
+    };
+    // Initialise the state that we want to live on the audio thread.
+    let stream = audio_host
+        .new_output_stream(audio_model)
+        .render(audio)
+        .sample_rate(96000)
+        .build()
+        .unwrap();
+
+    stream.play().unwrap();
+
     Model {
-        audio_host,
-        streams: Vec::new(),
+        stream,
+        clips: Vec::new(),
+        receiver: rx,
     }
 }
 
 fn audio(audio: &mut Audio, buffer: &mut Buffer) {
-    if !audio.is_completed {
-        let len_frames = buffer.len_frames();
+    let mut have_ended = vec![];
+    let len_frames = buffer.len_frames();
 
-        let sound = &mut audio.sound;
-        // Sum all of the sounds onto the buffer.
+    // Sum all of the sounds onto the buffer.
+    for (i, sound) in audio.sounds.iter_mut().enumerate() {
         let mut frame_count = 0;
-        let file_frames = sound.frames::<[f32; 2]>().filter_map(Result::ok);
+        let file_frames = sound.reader.frames::<[f32; 2]>().filter_map(Result::ok);
         for (frame, file_frame) in buffer.frames_mut().zip(file_frames) {
             for (sample, file_sample) in frame.iter_mut().zip(&file_frame) {
                 *sample += *file_sample;
@@ -53,61 +88,106 @@ fn audio(audio: &mut Audio, buffer: &mut Buffer) {
 
         // If the sound yielded less samples than are in the buffer, it must have ended.
         if frame_count < len_frames {
-            audio.is_completed = true;
-            println!("Sound ended!");
-            (audio.on_completed)();
+            println!("Clip {} ended", sound.id);
+            have_ended.push((
+                i,
+                AudioClipInfo {
+                    id: sound.id,
+                    state: AudioState::Complete(),
+                },
+            ));
+            sound.state = AudioState::Complete();
+        } else {
+            let progress = frame_count.to_f32().unwrap() / len_frames.to_f32().unwrap();
+            sound.state = AudioState::Playing(progress);
         }
-    }
-}
 
-fn simple_callback() {
-    println!("Boo!");
+        // if sound.state != AudioState::Complete() {
+        //     let progress = frame_count.to_f32().unwrap() / len_frames.to_f32().unwrap();
+        //     let clip_info = AudioClipInfo {
+        //         id: sound.id,
+        //         state: AudioState::Playing(progress),
+        //     };
+        //     audio.sender.send(clip_info).unwrap();
+        //     sound.state = AudioState::Playing(progress);
+        // }
+    }
+
+    // Remove all sounds that have ended.
+    for (i, clip_info) in have_ended.into_iter().rev() {
+        println!("Removing and notifying for completed clip #{}", i);
+        audio.sounds.remove(i);
+        audio.sender.send(clip_info).unwrap();
+    }
+
+    // for sound in &audio.sounds {
+    //     if let AudioState::Playing(progress) = sound.state {
+    //         let clip_info = AudioClipInfo {
+    //             id: sound.id,
+    //             state: AudioState::Playing(progress),
+    //         };
+    //         audio.sender.send(clip_info).unwrap();
+    //     }
+    // }
 }
 
 fn key_pressed(app: &App, model: &mut Model, key: Key) {
     if key == Key::Space {
         let assets = app.assets_path().expect("could not find assets directory");
         let path = assets.join("sounds").join("frog.wav");
-        if let Ok(sound) = audrey::open(path) {
-            let audio_model = Audio {
-                sound,
-                is_completed: false,
-                on_completed: simple_callback,
+        if let Ok(reader) = audrey::open(path) {
+            let new_clip = AudioClip {
+                id: model.clips.len(),
+                state: AudioState::Ready(),
+                reader,
             };
-            // Initialise the state that we want to live on the audio thread.
-            let stream = model
-                .audio_host
-                .new_output_stream(audio_model)
-                .render(audio)
-                .sample_rate(96000)
-                .build()
-                .unwrap();
-
-            stream
+            model.clips.push(AudioClipInfo {
+                id: model.clips.len(),
+                state: AudioState::Ready(),
+            });
+            model
+                .stream
                 .send(move |audio| {
-                    // audio.sound = sound;
-                    println!("audio model: {}", audio.is_completed);
+                    audio.sounds.push(new_clip);
                 })
                 .ok();
-
-            model.streams.push(stream);
         } else {
             panic!("Failed to load sound");
         }
     }
 }
 
-fn update(app: &App, model: &mut Model, update: Update) {
-    // if let Some(completed) = model.streams.iter().find(|s| s.is_paused()) {
-    //     println!("Completed stream");
-    // }
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    if let Ok(received) = &model.receiver.try_recv() {
+        println!("Received state {:?} from #{}", received.state, received.id);
+
+        match received.state {
+            AudioState::Complete() => {
+                if let Some(to_remove) = model
+                    .clips
+                    .iter()
+                    .enumerate()
+                    .find(|(_i, clip)| clip.id == received.id)
+                {
+                    let (i, _info) = to_remove;
+                    model.clips.remove(i);
+                } else {
+                    panic!("No match for clip id {}", received.id);
+                }
+            }
+            AudioState::Playing(progress) => {
+                // println!("Clip #{} progress {}", received.id, progress);
+            }
+            AudioState::Ready() => {}
+        }
+    }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
 
     draw.background().color(DARKSLATEGREY);
-    draw.text(&format!("playing {} sounds", model.streams.len()));
+    draw.text(&format!("playing {} sounds", model.clips.len()));
 
     draw.to_frame(app, &frame).unwrap();
 }
