691a849a7521eb46c86526b3d08b1e7abf0ab33b
Stephen Buchanan <stephen.buchanan@random.studio>
2023-03-21 10:25:11 +0100

    sending progress, but sleeping thread not optimal

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,41 @@
+use std::time::Duration;
+
 use nannou::prelude::*;
 use nannou_audio as audio;
 use nannou_audio::Buffer;
 use rtrb::{Consumer, Producer, RingBuffer};
 
 struct Model {
-    consumer: Consumer<AudioClipInfo>,
+    consumer: Consumer<ClipUpdate>,
     stream: audio::Stream<Audio>,
-    clips: Vec<AudioClipInfo>,
+    clips: Vec<AudioClipMetadata>,
 }
 
 #[derive(Debug, PartialEq)]
-enum ClipState {
+enum PlaybackState {
     Ready(),
-    Playing(f32),
+    Playing(usize),
     Complete(),
 }
 
 struct AudioClip {
     id: usize,
     reader: audrey::read::BufFileReader,
+    frames_played: usize,
+}
+
+struct AudioClipMetadata {
+    id: usize,
+    length: usize,
+    state: PlaybackState,
 }
 
 /// ID of the clip, followed by "state"
-type AudioClipInfo = (usize, ClipState);
+type ClipUpdate = (usize, PlaybackState);
 
 struct Audio {
     sounds: Vec<AudioClip>,
-    producer: Producer<AudioClipInfo>,
+    producer: Producer<ClipUpdate>,
 }
 
 fn main() {
@@ -83,12 +92,24 @@ fn audio(audio: &mut Audio, buffer: &mut Buffer) {
 
         // If the sound yielded less samples than are in the buffer, it must have ended.
         if frame_count < len_frames {
+            while audio.producer.is_full() {
+                std::thread::sleep(Duration::from_millis(1));
+            }
             have_ended.push(i);
-            audio.producer.push((sound.id, ClipState::Complete()));
+            audio
+                .producer
+                .push((sound.id, PlaybackState::Complete()))
+                .unwrap();
         } else {
-            // TODO: calculate progress
-            // You may need to calculate total frames/samples (once) at load, save this,
-            // inc a counter of each frame and use this as fraction to calculate progress
+            sound.frames_played += frame_count;
+            if audio.producer.is_full() {
+                // Ignore
+            } else {
+                audio
+                    .producer
+                    .push((sound.id, PlaybackState::Playing(sound.frames_played)))
+                    .unwrap();
+            }
         }
     }
 
@@ -98,14 +119,32 @@ fn audio(audio: &mut Audio, buffer: &mut Buffer) {
     }
 }
 
+fn calculate_length(path: &std::path::Path) -> usize {
+    let mut reader = audrey::open(path).unwrap();
+    let mut count = 0;
+    reader.frames::<[f32; 2]>().for_each(|_f| count += 1);
+    count
+}
+
 fn key_pressed(app: &App, model: &mut Model, key: Key) {
     if key == Key::Space {
         let assets = app.assets_path().expect("could not find assets directory");
         let path = assets.join("sounds").join("frog.wav");
-        if let Ok(reader) = audrey::open(path) {
+        if let Ok(reader) = audrey::open(&path) {
+            println!("Opened sound file OK: {:?}", reader.description());
+            let length = calculate_length(&path);
+            println!("Got length {}", length);
             let id = model.clips.len();
-            let new_clip = AudioClip { id, reader };
-            model.clips.push((id, ClipState::Ready()));
+            let new_clip = AudioClip {
+                id,
+                reader,
+                frames_played: 0,
+            };
+            model.clips.push(AudioClipMetadata {
+                id,
+                length,
+                state: PlaybackState::Ready(),
+            });
             model
                 .stream
                 .send(move |audio| {
@@ -122,48 +161,35 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
     if let Ok(receive) = model.consumer.pop() {
         let (id, state) = receive;
         match state {
-            ClipState::Complete() => {
+            PlaybackState::Complete() => {
                 if let Some(to_remove) = model
                     .clips
                     .iter()
                     .enumerate()
-                    .find(|(_i, clip_info)| clip_info.0 == id)
+                    .find(|(_i, clip_meta)| clip_meta.id == id)
                 {
                     let (i, _info) = to_remove;
+                    println!("Removing clip at index {}", i);
                     model.clips.remove(i);
                 } else {
                     panic!("No match for clip id {}", id);
                 }
             }
-            ClipState::Playing(progress) => {
-                // println!("Clip #{} progress {}", received.id, progress);
+            PlaybackState::Playing(frames_played) => {
+                println!("Got Playing state: {}", frames_played);
+                if let Some(to_update) = model
+                    .clips
+                    .iter()
+                    .enumerate()
+                    .find(|(_i, clip_meta)| clip_meta.id == id)
+                {
+                    let (index, _c) = to_update;
+                    model.clips[index].state = PlaybackState::Playing(frames_played);
+                }
             }
-            ClipState::Ready() => {}
+            PlaybackState::Ready() => {}
         }
     }
-    // if let Ok(received) = &model.consumer.try_recv() {
-    //     println!("Received state {:?} from #{}", received.state, received.id);
-
-    //     match received.state {
-    //         ClipState::Complete() => {
-    //             if let Some(to_remove) = model
-    //                 .clips
-    //                 .iter()
-    //                 .enumerate()
-    //                 .find(|(_i, clip)| clip.id == received.id)
-    //             {
-    //                 let (i, _info) = to_remove;
-    //                 model.clips.remove(i);
-    //             } else {
-    //                 panic!("No match for clip id {}", received.id);
-    //             }
-    //         }
-    //         ClipState::Playing(progress) => {
-    //             // println!("Clip #{} progress {}", received.id, progress);
-    //         }
-    //         ClipState::Ready() => {}
-    //     }
-    // }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
@@ -171,6 +197,15 @@ fn view(app: &App, model: &Model, frame: Frame) {
 
     draw.background().color(DARKSLATEGREY);
     draw.text(&format!("playing {} sounds", model.clips.len()));
+
+    let start_y = -15.;
+    for (i, c) in model.clips.iter().enumerate() {
+        if let PlaybackState::Playing(progress) = c.state {
+            draw.text(&format!("clip ID#{} : {} / {}", c.id, progress, c.length))
+                .left_justify()
+                .y(start_y - (i * 15).to_f32().unwrap());
+        }
+    }
 
     draw.to_frame(app, &frame).unwrap();
 }
