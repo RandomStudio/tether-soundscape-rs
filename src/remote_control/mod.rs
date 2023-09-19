269aa3acaf0638029d79922330c57aba82b289ec
stephen <stephen.buchanan@random.studio>
2023-09-19 14:52:11 +0200

    better way to init hashmap

--- src/remote_control/mod.rs
+++ src/remote_control/mod.rs
@@ -25,41 +25,37 @@ impl RemoteControl {
         state_send_interval: Duration,
         state_max_empty: usize,
     ) -> Self {
-        let mut input_plugs: HashMap<String, PlugDefinition> = HashMap::new();
-
         let id = match override_subscribe_id {
             Some(s) => String::from(s),
             None => String::from("+"),
         };
 
-        input_plugs.insert(
-            "clipCommands".into(),
-            PlugOptionsBuilder::create_input("clipCommands")
-                .qos(2)
-                .topic(&build_topic("+".into(), &id, "clipCommands"))
-                .build(tether_agent)
-                .expect("failed to create clipCommands Input"), // tether_agent
-                                                                //     .create_input_plug("clipCommands", Some(2), None)
-                                                                //     .unwrap(),
-        );
-        input_plugs.insert(
-            "scenes".into(),
-            PlugOptionsBuilder::create_input("scenes")
-                .qos(2)
-                .topic(&build_topic("+".into(), &id, "scenes"))
-                .build(tether_agent)
-                .expect("failed to create scenes Input"), // tether_agent
-                                                          //     .create_input_plug("scenes", Some(2), None)
-                                                          //     .unwrap(),
-        );
-        input_plugs.insert(
-            "globalControls".into(),
-            PlugOptionsBuilder::create_input("globalControls")
-                .qos(2)
-                .topic(&build_topic("+".into(), &id, "globalControls"))
-                .build(tether_agent)
-                .expect("failed to create globalCommands Input"), // tether_agent
-        );
+        let input_plugs: HashMap<String, PlugDefinition> = HashMap::from([
+            (
+                "clipCommands".into(),
+                PlugOptionsBuilder::create_input("clipCommands")
+                    .qos(2)
+                    .topic(&build_topic("+".into(), &id, "clipCommands"))
+                    .build(tether_agent)
+                    .expect("failed to create clipCommands Input"), // tether_agent
+            ),
+            (
+                "scenes".into(),
+                PlugOptionsBuilder::create_input("scenes")
+                    .qos(2)
+                    .topic(&build_topic("+".into(), &id, "scenes"))
+                    .build(tether_agent)
+                    .expect("failed to create scenes Input"), // tether_agent
+            ),
+            (
+                "globalControls".into(),
+                PlugOptionsBuilder::create_input("globalControls")
+                    .qos(2)
+                    .topic(&build_topic("+".into(), &id, "globalControls"))
+                    .build(tether_agent)
+                    .expect("failed to create globalCommands Input"), // tether_agent
+            ),
+        ]);
 
         let state_output_plug = PlugOptionsBuilder::create_output("state")
             .qos(0)
